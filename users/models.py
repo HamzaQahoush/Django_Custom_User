0e6dc88f04a3cd8e77753d0cc18a39f2d6f59988
HamzaQahoush <hamza.qah@gmail.com>
2021-07-14 18:18:28 +0300

    do tests

--- users/models.py
+++ users/models.py
@@ -4,5 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
+
+    USERNAME_FIELD = "email"
+    email = models.EmailField(
+        ("email address"), unique=True
+    )  # changes email to unique and blank to false
+    REQUIRED_FIELDS = ["username"]  # removes email from REQUIRED_FIELDS
+
     def __str__(self):
         return self.username
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+# from .models import users
+from .models import CustomUser
+
+
+class userTests(TestCase):
+    def setUp(self):
+        self.users = get_user_model().objects.create_user(
+            username="Hamza", email="hamza.qah@gmail.com", password="pwd"
+        )
+
+    def test_user_creation(self):
+        email = self.users.email
+        self.assertEquals(self.users.__str__(), "Hamza")
+        self.assertEquals(self.users.email.__str__(), "hamza.qah@gmail.com")
+
+    def test_duplicates(self):
+        try:
+            self.user = get_user_model().objects.create_user(
+                username="Hamza", email="hamza.qah@gmail.com", password="pwd"
+            )
+        except:
+            print("Resgistration falied!")
